4d64a590f5110640ee7cf5c79fc0cc481b66365d
Opilane TTHK <opilane@tthk.ee>
2021-09-28 10:07:24 +0300

    commit

---
+++ LoginCredentials/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LoginCredentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Login Form
+             * login: user
+             * password: user1234
+             * Create a program that asks user's login credentials
+             * Check if the user entered correct login and password
+             * If the login and password are correct display 'Welcome'
+             * If the login or password are incorrect dispay 'try again'
+             
+             */
+            string login;
+            string password;
+            Console.WriteLine("Login: ");
+            login = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            password = Console.ReadLine();
+
+            if ((login == "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("Welcome.");
+            }
+            else if ((login == "admin") && (password != "pass1234"))
+            {
+                Console.WriteLine("The password is incorrect");
+            }
+            else if ((login != "admin") && (password == "pass1234"))
+            {
+                Console.WriteLine("Welcome.");
+            }
+            else
+            {
+                Console.WriteLine("Success!");
+            }
+        }
+    }
+}
